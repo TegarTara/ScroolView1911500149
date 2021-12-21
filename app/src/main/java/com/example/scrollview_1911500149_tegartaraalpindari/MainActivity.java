fd1fa3934e9de3b871955860465c608c29fd9a5f
TegarTara <tegartara769@gmail.com>
2021-12-21 13:27:38 +0700

    upload

---
+++ app/src/main/java/com/example/scrollview_1911500149_tegartaraalpindari/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.scrollview_1911500149_tegartaraalpindari;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
